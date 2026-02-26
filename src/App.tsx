0c9efacc7c2a2b8f83754467c38e198e1f7ec566
Carolina Hatakeyama <carolinahatakeyama2@gmail.com>
2026-02-26 17:31:32 -0300

    Tipagem typescript de children

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
-  console.log("Oi");
-
   return (
     <>
-      <Heading />
+      <Heading>
+        Ola mundo 1
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>Lorem</p>
     </>
   );
